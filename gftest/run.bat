6636e23ac91958ad8228bce164824708836acc85
unknown <ksc@.(none)>
2011-06-02 19:33:00 +0900

    gradle test

--- gftest/run.bat
+++ gftest/run.bat
@@ -1,8 +1,11 @@
-set GROOVY_HOME=c:\opt\groovy-1.7.10
-set GRAILS_HOME=C:\opt\grails-1.3.7
 set JAVA_HOME=c:\opt\jdk
+set GROOVY_HOME=C:\opt\Groovy-1.7.10
+set GRIFFON_HOME=C:\opt\griffon-0.9.2
+set GRAILS_HOME=C:\opt\grails-1.3.7
+set GRADLE_HOME=D:\Tooldev\gradle-1.0-milestone-3
 
-set PATH=%GROOVY_HOME%/bin;%GRAILS_HOME%/bin;%JAVA_HOME%/bin
+set PATH=%GRIFFON_HOME%/bin;%JAVA_HOME%/bin;%GROOVY_HOME%/bin;%GRAILS_HOME%/bin;%GRADLE_HOME%/bin
 
-grails run-app > hogehoge.txt 2>&1
-::grails run-app
+grails run-app
+::gradle run-app --info> hogehoge.txt 2>&1
+
