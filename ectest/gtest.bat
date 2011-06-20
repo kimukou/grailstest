33c64578a65b9f704ca1c45c7cc973653cab1499
unknown <ksc@.(none)>
2011-06-20 20:56:40 +0900

    update gradle opt

---
+++ ectest/gtest.bat
@@ -0,0 +1,12 @@
+set JAVA_HOME=c:\opt\jdk
+set GROOVY_HOME=C:\opt\Groovy-1.8.0
+set GRIFFON_HOME=C:\opt\griffon-0.9.2
+set GRAILS_HOME=C:\opt\grails-1.3.7
+set GRADLE_HOME=D:\Tooldev\gradle-1.0-milestone-3
+
+set PATH=%GRIFFON_HOME%/bin;%JAVA_HOME%/bin;%GROOVY_HOME%/bin;%GRAILS_HOME%/bin;%GRADLE_HOME%/bin
+set JAVA_OPTS=-Dgroovy.source.encoding=UTF-8 -Dfile.encoding=UTF-8
+
+::gradle test > hogehoge.txt 2>&1
+::gradle war -PcmdOpt="ectest.zip"  > hogehoge.txt 2>&1
+gradle war > hogehoge.txt 2>&1
