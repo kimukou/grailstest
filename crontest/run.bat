d6027967f80082e76e2b9d52e41eb1a3305f524f
kimukou <kimukou.buzz@gmail.com>
2011-08-01 20:37:14 +0900

    init

---
+++ crontest/run.bat
@@ -0,0 +1,5 @@
+call ../setEnvG9.bat
+
+grails run-app -Dserver.port=8090
+::gradle run-app -Dserver.port=8090 --info> hogehoge.txt 2>&1
+
---
+++ crontest/test/unit/crontest/BackupJobJobTests.groovy
@@ -0,0 +1,17 @@
+package crontest
+
+import grails.test.*
+
+class BackupJobJobTests extends GrailsUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
